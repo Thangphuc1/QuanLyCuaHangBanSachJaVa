0d120c57af0542a0f751bf5b67487f7ed02d4940
Daviantt <gxtq64@gmail.com>
2026-02-11 15:37:36 +0700

    remodel file

---
+++ src/PhieuNhap/PhieuNhapDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PhieuNhap;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PhieuNhapDAO {
+    
+}
