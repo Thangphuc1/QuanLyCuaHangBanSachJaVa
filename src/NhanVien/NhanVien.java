090febaac8fbc1f87cde62363a9d6c252d356f5a
phuongpeka-cell <phuongpeka@gmail.com>
2026-02-26 21:12:08 +0700

    Update NhanVienDAO , NhanVien

--- src/NhanVien/NhanVien.java
+++ src/NhanVien/NhanVien.java
@@ -4,10 +4,76 @@
  */
 package NhanVien;
 
+import java.util.Objects;
+
 /**
  *
- * @author ASUS
+ * @author DuyPhuong
  */
 public class NhanVien {
-    
+    private String maNV , tenNV , hoNV , sdt , diaChi , email ;
+    private double luong;
+
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NhanVien nv)) return false;
+        return Objects.equals(maNV, nv.maNV);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maNV);
+    }
+    public String getMaNV(){
+        return maNV;
+    }
+    public void setMaNV(String maNV){
+        this.maNV = maNV;
+    }
+    public String getTenNV(){
+        return tenNV;
+    }
+    public void setTenNV(String maNV){
+        this.maNV=maNV;
+    }
+    public String getHoNV(){
+        return hoNV;
+    }
+    public void setHoNV(String hoNV){
+        this.hoNV=hoNV;
+    }
+    public String getSDT(){
+        return sdt;
+    }
+    public void setSDT(String sdt){
+        this.sdt=sdt;
+    }
+    public String getDiaChi(){
+        return diaChi;
+    }
+    public void setDiaChi(String diaChi){
+        this.diaChi=diaChi;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public double getLuong (){
+        return luong;
+    }
+    public void setLuong(double luong){
+        this.luong=luong;
+    }
+    public NhanVien(String maNV , String tenNV , String hoNV , String sdt, String diaChi , String email , double luong){
+        this.maNV=maNV;
+        this.tenNV=tenNV;
+        this.hoNV=hoNV;
+        this.sdt=sdt;
+        this.diaChi=diaChi;
+        this.email=email;
+        this.luong=luong;
+    }
 }
--- src/NhanVien/NhanVienDAO.java
+++ src/NhanVien/NhanVienDAO.java
@@ -3,11 +3,94 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package NhanVien;
-
+    import java.sql.Connection;
+    import java.sql.PreparedStatement;
+    import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 /**
  *
  * @author ASUS
  */
 public class NhanVienDAO {
-    
+    public ArrayList<NhanVien> loadNhanVien(){
+        String qry = "select * from NhanVien";
+        ArrayList<NhanVien> dsNhanVien = new ArrayList();
+        try(Connection conn = DBConnection.getDBConnection();
+            PreparedStatement st = conn.prepareStatement(qry);
+            ResultSet rs = st.executeQuery();){
+            
+            while(rs.next()){
+                        NhanVien nhanVien = new NhanVien(
+                            rs.getString("MaNhanVien"),
+                            rs.getString("TenNhanVien"),
+                            rs.getString("HoNhanVien"),
+                            rs.getString("DiaChi"),
+                            rs.getString("SDT"),
+                            rs.getString("Email"),
+                            rs.getDouble("Luong")
+);
+                dsNhanVien.add(nhanVien);
+            }
+            
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        return dsNhanVien;
+    } 
+    public boolean insertNhanVien(NhanVien nhanVien){
+        String qry = "insert into NhanVien values (?,?,?,?,?)";
+        try(Connection conn = DBConnection.getDBConnection();
+            PreparedStatement st = conn.prepareStatement(qry);){
+            
+            st.setString(1, nhanVien.getMaNV());
+            st.setString(2, nhanVien.getTenNV());
+            st.setString(3, nhanVien.getHoNV());
+            st.setString(4, nhanVien.getDiaChi());
+            st.setString(5, nhanVien.getSDT());
+            st.setString(6, nhanVien.getEmail());
+            st.setDouble(7, nhanVien.getLuong());
+            
+            st.executeUpdate();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    public boolean deleteNhanVien(String ma){
+        String qry = "Delete from nhaxuatban where MaNV = ?";
+        try(Connection conn = DBConnection.getDBConnection();
+            PreparedStatement st = conn.prepareStatement(qry);){
+            
+            st.setString(1, ma);
+            
+            st.executeUpdate();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    public boolean updateNhanVien(NhanVien nhanVien){
+        String qry = "update hhanhanvien set maNV = ?, TenNV = ?, DiaChi = ?, SDT = ?, Email = ? where MaNV = ?";
+        try(Connection conn = DBConnection.getDBConnection();
+            PreparedStatement st = conn.prepareStatement(qry);){
+            
+            st.setString(1, nhanVien.getMaNV());
+            st.setString(2, nhanVien.getTenNV());
+            st.setString(3, nhanVien.getHoNV());
+            st.setString(4, nhanVien.getDiaChi());
+            st.setString(5, nhanVien.getSDT());
+            st.setString(6, nhanVien.getEmail());
+            st.setDouble(7, nhanVien.getLuong());
+            
+            st.executeUpdate();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
+
