94f6a849ad87c0d5940dc0e682cef4039eb5289a
Quocanh0201 <voquocanh0201@gmail.com>
2026-02-27 11:25:31 +0700

    Update HoaDon and ChiTietHoaDon

--- src/ChiTietHoaDon/ChiTietHoaDon.java
+++ src/ChiTietHoaDon/ChiTietHoaDon.java
@@ -9,5 +9,54 @@ package ChiTietHoaDon;
  * @author ASUS
  */
 public class ChiTietHoaDon {
+    private String maHD;
+    private String maSach;
+    private int soLuong;
+    private double donGia;
+    private double thanhTien;
+    public ChiTietHoaDon() {}
+    public ChiTietHoaDon (String maHD,String maSach,int soLuong,double donGia,double thanhTien) {
+        this.maHD=maHD;
+        this.maSach=maSach;
+        this.soLuong=soLuong;
+        this.donGia=donGia;
+        this.thanhTien=thanhTien;
+    }
+
+    public String getMaPN() {
+        return maHD;
+    }
+
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public double getThanhTien() {
+        return thanhTien;
+    }
+
+    public void setMaPN(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
     
 }
--- src/HoaDon/HoaDon.java
+++ src/HoaDon/HoaDon.java
@@ -4,10 +4,62 @@
  */
 package HoaDon;
 
-/**
- *
- * @author ASUS
- */
+import java.time.LocalDate;
+
+
 public class HoaDon {
+    private String maHD;
+    private String maKH;
+    private String maNV;
+    private LocalDate ngayLap;
+    private Double tongTien;
+    public HoaDon() {}
+    public HoaDon(String maHD,String maKH,String maNV,LocalDate ngayLap,Double tongTien) {
+        this.maHD=maHD;
+        this.maKH=maKH;
+        this.maNV=maNV;
+        this.ngayLap=ngayLap;
+        this.tongTien=tongTien;
+    }
+
+    public String getMaPN() {
+        return maHD;
+    }
+
+    public String getMaNCC() {
+        return maKH;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public LocalDate getNgayLap() {
+        return ngayLap;
+    }
+
+    public Double getTongTien() {
+        return tongTien;
+    }
+
+    public void setMaPN(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public void setMaNCC(String maKH) {
+        this.maKH = maKH;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public void setNgayLap(LocalDate ngayLap) {
+        this.ngayLap = ngayLap;
+    }
+
+    public void setTongTien(Double tongTien) {
+        this.tongTien = tongTien;
+    }
     
 }
