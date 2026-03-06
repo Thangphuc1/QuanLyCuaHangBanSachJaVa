ebfe12a16685b7e6d2932b0b9637edbaef042f4b
KimChi006 <ntkchi92lhp2021@gmail.com>
2026-03-06 16:49:34 +0700

    Add files via upload

--- src/KhuyenMai/KhuyenMaiBUS.java
+++ src/KhuyenMai/KhuyenMaiBUS.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package KhuyenMai;
 
-/**
- *
- * @author ASUS
- */
+import java.util.ArrayList;
+
 public class KhuyenMaiBUS {
-    
+
+    private KhuyenMaiDAO kmDAO;
+
+    public KhuyenMaiBUS() {
+        kmDAO = new KhuyenMaiDAO();
+    }
+
+    // Lấy danh sách khuyến mãi
+    public ArrayList<KhuyenMai> getAll() {
+        return kmDAO.getAll();
+    }
+
+    // Thêm khuyến mãi
+    public boolean add(KhuyenMai km) {
+
+        if (km.getMaKM().trim().equals("")) {
+            return false;
+        }
+
+        return kmDAO.insert(km);
+    }
+
+    // Sửa khuyến mãi
+    public boolean update(KhuyenMai km) {
+
+        if (km.getMaKM().trim().equals("")) {
+            return false;
+        }
+
+        return kmDAO.update(km);
+    }
+
+    // Xóa khuyến mãi
+    public boolean delete(String maKM) {
+
+        if (maKM.trim().equals("")) {
+            return false;
+        }
+
+        return kmDAO.delete(maKM);
+    }
 }
