0356938df1ce620892a29df0d971f8313b717a8f
KimChi006 <ntkchi92lhp2021@gmail.com>
2026-03-09 15:21:02 +0700

    Add files via upload

---
+++ src/CHiTietKhuyenMaiSP/CHiTietKhuyenMaiSPBUS.java
@@ -0,0 +1,47 @@
+package CHiTietKhuyenMaiSP;
+
+import java.util.ArrayList;
+
+public class CHiTietKhuyenMaiSPBUS {
+
+    private CHiTietKhuyenMaiSPDAO ctkmDAO;
+
+    public CHiTietKhuyenMaiSPBUS() {
+        ctkmDAO = new CHiTietKhuyenMaiSPDAO();
+    }
+
+    // Lấy danh sách
+    public ArrayList<CHiTietKhuyenMaiSP> getAll() {
+        return ctkmDAO.getAll();
+    }
+
+    // Thêm
+    public boolean add(CHiTietKhuyenMaiSP km) {
+
+        if (km.getMaKM().trim().equals("") || km.getMaSP().trim().equals("")) {
+            return false;
+        }
+
+        return ctkmDAO.insert(km);
+    }
+
+    // Sửa
+    public boolean update(CHiTietKhuyenMaiSP km) {
+
+        if (km.getMaKM().trim().equals("") || km.getMaSP().trim().equals("")) {
+            return false;
+        }
+
+        return ctkmDAO.update(km);
+    }
+
+    // Xóa
+    public boolean delete(String maKM, String maSP) {
+
+        if (maKM.trim().equals("") || maSP.trim().equals("")) {
+            return false;
+        }
+
+        return ctkmDAO.delete(maKM, maSP);
+    }
+}
---
+++ src/CHiTietKhuyenMaiSP/CHiTietKhuyenMaiSPDAO.java
@@ -0,0 +1,110 @@
+package CHiTietKhuyenMaiSP;
+
+import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class CHiTietKhuyenMaiSPDAO {
+
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    // Lấy danh sách
+    public ArrayList<CHiTietKhuyenMaiSP> getAll() {
+
+        ArrayList<CHiTietKhuyenMaiSP> list = new ArrayList<>();
+        String sql = "SELECT * FROM ChiTietKhuyenMaiSP";
+
+        try {
+
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                CHiTietKhuyenMaiSP ctkm = new CHiTietKhuyenMaiSP(
+                        rs.getString("maKM"),
+                        rs.getString("maSP"),
+                        rs.getFloat("phamtramGG")
+                );
+
+                list.add(ctkm);
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Lỗi đọc dữ liệu ChiTietKhuyenMaiSP");
+        }
+
+        return list;
+    }
+
+    // Thêm
+    public boolean insert(CHiTietKhuyenMaiSP ctkm) {
+
+        String sql = "INSERT INTO ChiTietKhuyenMaiSP VALUES(?,?,?)";
+
+        try {
+
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sql);
+
+            ps.setString(1, ctkm.getMaKM());
+            ps.setString(2, ctkm.getMaSP());
+            ps.setFloat(3, ctkm.getPhantramgg());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Lỗi thêm ChiTietKhuyenMaiSP");
+        }
+
+        return false;
+    }
+
+    // Sửa
+    public boolean update(CHiTietKhuyenMaiSP ctkm) {
+
+        String sql = "UPDATE ChiTietKhuyenMaiSP SET phamtramGG=? WHERE maKM=? AND maSP=?";
+
+        try {
+
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sql);
+
+            ps.setFloat(1, ctkm.getPhantramgg());
+            ps.setString(2, ctkm.getMaKM());
+            ps.setString(3, ctkm.getMaSP());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Lỗi sửa ChiTietKhuyenMaiSP");
+        }
+
+        return false;
+    }
+
+    // Xóa
+    public boolean delete(String maKM, String maSP) {
+
+        String sql = "DELETE FROM ChiTietKhuyenMaiSP WHERE maKM=? AND maSP=?";
+
+        try {
+
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sql);
+
+            ps.setString(1, maKM);
+            ps.setString(2, maSP);
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Lỗi xoá ChiTietKhuyenMaiSP");
+        }
+
+        return false;
+    }
+}
