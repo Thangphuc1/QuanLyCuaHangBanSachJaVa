50f43c901bbb699b8b7773a66d7e9c30f24945d3
Thangphuc1 <3124410331@sv.sgu.edu.vn>
2026-03-26 20:12:24 +0700

    update ncc

--- src/ChiTietPhieuNhap/ChiTietPhieuNhapBUS.java
+++ src/ChiTietPhieuNhap/ChiTietPhieuNhapBUS.java
@@ -1,13 +1,101 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ChiTietPhieuNhap;
+import PhieuNhap.*;
+import Sach.*;
 
 /**
  *
  * @author ASUS
  */
+import Sach.SachDAO;
+import java.util.ArrayList;
 public class ChiTietPhieuNhapBUS {
+    private ChiTietPhieuNhapDAO ctdao=new ChiTietPhieuNhapDAO();
+    private PhieuNhapDAO pndao=new PhieuNhapDAO();
+    private SachDAO sdao=new SachDAO();
+    ArrayList<ChiTietPhieuNhap> dsctpn=new ArrayList<>();
+    ArrayList<PhieuNhap> dspn=new ArrayList<>();
+    ArrayList<Sach> dss=new ArrayList<>();
     
+    public ArrayList<ChiTietPhieuNhap> GetChiTietPhieuNhap(ChiTietPhieuNhap ctpn) {
+        return ctdao.GetByPhieuNhap(ctpn.getMaPN());
+    }
+    public boolean ThemChiTietPhieuNhap(ChiTietPhieuNhap ctpn) {
+        if(!KiemTra(ctpn)) {
+            System.out.println("vui long dien day du thong tin, khong bo trong bat ky o nao !");
+            return false;
+        }
+        if(!KiemTraMaPNTonTai(ctpn.getMaPN())) {
+            System.out.println("ma phieu nhap ko ton tai");
+            return false;
+        }
+        if(!KiemTraMaSachTonTai(ctpn.getMaSach())) {
+            System.out.println("ma sach ko ton tai");
+            return false;
+        }
+        boolean KetQua=ctdao.InsertChiTietPhieuNhap(ctpn);
+        if(KetQua) {
+            dsctpn=ctdao.LoadChiTietPhieuNhap();
+        }
+        return KetQua;
+    }
+    public boolean SuaChiTietPhieuNhap(ChiTietPhieuNhap ctpn) {
+        if(!KiemTra(ctpn)) {
+            System.out.println("vui long dien day du thong tin, khong bo trong bat ky o nao !");
+            return false;
+        }
+        boolean KetQua=ctdao.UpdateChiTietPhieuNhap(ctpn);
+        if(KetQua) {
+            dsctpn=ctdao.LoadChiTietPhieuNhap();
+        }
+        return KetQua;
+    }
+    public boolean XoaChiTietPhieuNhap(ChiTietPhieuNhap ctpn) {
+        boolean KetQua=ctdao.DeleteChiTietPhieuNhap(ctpn);
+        if(KetQua) {
+            dsctpn=ctdao.LoadChiTietPhieuNhap();
+        }
+        return KetQua;
+    }
+    
+    
+//=================VALIDATE==============
+    public boolean KiemTra(ChiTietPhieuNhap ctpn) {
+        if(ctpn.getMaPN()==null || ctpn.getMaSach()==null) {
+            return false;
+        }
+        if(ctpn.getMaPN().isEmpty() || ctpn.getMaSach().isEmpty()) {
+            return false;
+        }
+        if (ctpn.getSoLuong()<=0) {
+            return false;
+        }
+        if(ctpn.getDonGia()<=0.0 ) {
+            return false;
+        }
+        if(ctpn.getThanhTien()<=0.0) {
+            return false;
+        }
+        return true;
+    }
+    public boolean KiemTraMaPNTonTai(String mapn) {
+        for(PhieuNhap pn : dspn) {
+            if(pn.getMaPN().equals(mapn)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean KiemTraMaSachTonTai(String mas) {
+        for(Sach s : dss) {
+            if(s.getMasach().equalsIgnoreCase(mas)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // trong 1 phieu nhap ko the nao co 2 dong chi tiet phieu nhap giong nhau, nhapp cung 1 loai sach(sai logic)
 }
--- src/NhaCungCap/NhaCungCapBUS.java
+++ src/NhaCungCap/NhaCungCapBUS.java
@@ -5,6 +5,8 @@
 package NhaCungCap;
 import PhieuNhap.*;
 import java.util.ArrayList;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -22,21 +24,19 @@ public class NhaCungCapBUS {
     public ArrayList<NhaCungCap> getNhaCungCap() {
         return dsncc;
     }
-    public boolean themNhaCungCap(NhaCungCap ncc) {
-       if(KiemTra(ncc)) {
-           System.out.println("vui long kiem tra da dien du thong tin va nhap du lieu phu hop");
-           return false;
-       }
-  
+    public boolean themNhaCungCap(NhaCungCap ncc,JDialog parentDialog)  {
+        if(!KiemTra(ncc, parentDialog))
+                return false;
+        
 
        if(KiemTraTenTrung(ncc.getTenNCC())) {
-           System.out.println("ten nha cung cap bi trung");
+           JOptionPane.showMessageDialog(parentDialog,"tên nhà cung cấp đã bị trùng!","Lỗi",JOptionPane.ERROR_MESSAGE);
            return false;
         }
        if(KiemTraMaTonTai(ncc.getMaNCC())) {
-           System.out.println("ma nha cung cap da ton tai");
-           return false;
+           JOptionPane.showMessageDialog(parentDialog,"mã nhà cung cấp đã bị trùng!","Lỗi",JOptionPane.ERROR_MESSAGE);    
        }
+       
        boolean KetQua=nhacungcapdao.InsertNhaCungCap(ncc);
        if(KetQua) {
            dsncc.add(ncc);
@@ -58,7 +58,7 @@ public class NhaCungCapBUS {
         if(!KiemTraMaTonTai(ncc.getMaNCC())) {
             System.out.println("ma khong ton tai");
         }
-        KiemTra(ncc);
+//        if(KiemTra(ncc.);
         boolean KetQua=nhacungcapdao.UpdateNhaCungCap(ncc);
         if (KetQua) dsncc = nhacungcapdao.LoadNhaCungCap();
         return KetQua ;
@@ -85,18 +85,18 @@ public class NhaCungCapBUS {
         
     
 //=======================VALIDATION==================    
-    public boolean  KiemTra(NhaCungCap ncc) {
+    public boolean  KiemTra(NhaCungCap ncc, JDialog parentDialog) {
         //kiem tra rong
         if(ncc.getTenNCC()==null||ncc.getMaNCC()==null||ncc.getDiaChi()==null||ncc.getEmail()==null) {
-            System.out.println("vui long dien thong tin day du");
+            JOptionPane.showMessageDialog(parentDialog,"vui lòng điền đầy đủ thông tin!","Lỗi",JOptionPane.ERROR_MESSAGE);
             return false;
         }
         if(ncc.getTenNCC().isEmpty()||ncc.getMaNCC().isEmpty()||ncc.getSoDienThoai().isEmpty()||ncc.getDiaChi().isEmpty()||ncc.getEmail().isEmpty()) {
-            System.out.println("vui long ko de trong");
+            JOptionPane.showMessageDialog(parentDialog,"vui lòng không để trống!","Lỗi",JOptionPane.ERROR_MESSAGE);
             return false;
         }
-        if(ncc.getSoDienThoai()==null||ncc.getSoDienThoai().length()!=10) {
-            System.out.println("so dien thoai phai bang 10 so");
+        if(ncc.getSoDienThoai()==null||ncc.getSoDienThoai().length()<10) {
+            JOptionPane.showMessageDialog(parentDialog,"số điện thoại phải có 10 chữ số!","Lỗi",JOptionPane.ERROR_MESSAGE);
             return false;
         }
         
@@ -118,6 +118,10 @@ public class NhaCungCapBUS {
             }
         }
         return false;
+    }
+
+    boolean xoaNhaCungCap(NhaCungCap ncc) {
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
     
     
